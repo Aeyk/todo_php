438e83146b8f6cc9079aba5858e28abc58adf905
Malik Kennedy <mksybr@gmail.com>
2022-03-17 08:29:20 -0400

    initial commit: infrastructure, containers

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>HTML5</title>
+</head>
+<body>
+  <?php
+  	include __DIR__.'/db.php';
+		phpinfo();
+	?>
+</body>
+</html>
