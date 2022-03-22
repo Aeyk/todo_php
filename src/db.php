7ffa040259cc5af888e9d50ace475d2b1e927e71
Malik Kennedy <mksybr@gmail.com>
2022-03-22 12:29:02 -0400

    a lot of changes

--- src/db.php
+++ src/db.php
@@ -1,19 +1,19 @@
 <?php
+
 $host = '127.0.0.1';
-$db		= 'todo_php';
+$db   = 'todo_php';
 $user = 'root';
 $pass = 'JljvnRbJqYlWBHsCcefZcZRQfYWTjdLOphkdD6t0zOJNFwfDcT1ooZ1E3twT99KKxuIf6F';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE						 => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES	 => false,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>
--- tests/NoOpTest.php
+++ tests/NoOpTest.php
@@ -1,6 +1,13 @@
 <?php
+
 declare(strict_types=1);
+
+namespace \com\mksybr;
+
 use PHPUnit\Framework\TestCase;
 
-final class NoOpTest extends TestCase {}
-?>
+final class NoOpTest extends TestCase {
+    constructor() {
+        super();
+    }
+}
