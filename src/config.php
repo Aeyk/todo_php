09b1589c068b6e8d871b6038ef28608c469e3c3b
mksybr@gmail.com <mksybr@gmail.com>
2022-04-23 21:40:20 -0400

    hello world :)

--- src/config.php
+++ src/config.php
@@ -1,6 +1,9 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
+// use Symfony\Component\Dotenv\Dotenv;
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
+// $dotenv = new Dotenv();
+// $dotenv->load(__DIR__ . '/.env');
+
+
+# define('SITE_ROOT', __DIR__);
--- src/index.php
+++ src/index.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="utf-8"/>
-     <title>HTML5</title>
-</head>
-<body>
-    <?php
-    include __DIR__ . '/db.php';
-    ?>
-</body>
-</html>
+<?php
+
+# set_include_path(get_include_path().":"."/var/www/html/vendor/");
+require '/opt/app/vendor/autoload.php'; // TODO(malik): put this into an env var
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/register', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("REGISTER");
+    return $response;
+});
+
+$app->run();
